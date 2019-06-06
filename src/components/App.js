295114733b8fd291983868370ed220af35012f9a
john <johnwee35@gmail.com>
2019-06-06 12:17:33 +0800

    updated card component name

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,3 @@
-
 /**
  * centralized component App for all components
  */
@@ -15,19 +14,18 @@ class App extends React.Component {
    * sets the initial fruits array value from the api
    */
   componentDidMount = async () => {
-    try{
-    const response = await fetch(
-      'https://my-json-server.typicode.com/Balancedsan/fruitsApi/fruits/'
-    )
-    const fruits = await response.json();
+    try {
+      const response = await fetch(
+        'https://my-json-server.typicode.com/Balancedsan/fruitsApi/fruits/'
+      );
+      const fruits = await response.json();
 
-    this.setState({
-      fruits: fruits
-    });
-    }catch(err){
+      this.setState({
+        fruits: fruits
+      });
+    } catch (err) {
       console.log(err);
     }
-
   };
 
   /**
@@ -43,12 +41,12 @@ class App extends React.Component {
    * render each card to the screen based on the input keyed  by the user
    */
   renderCards = () => {
-    if(this.state.textValue){
-      const filtered = this.state.fruits.filter(fruit=> {
+    if (this.state.textValue) {
+      const filtered = this.state.fruits.filter((fruit) => {
         return fruit.type.includes(this.state.textValue);
-      })
-      return this.createCards(filtered)
-    }else{
+      });
+      return this.createCards(filtered);
+    } else {
       return this.createCards(this.state.fruits);
     }
   };
@@ -57,19 +55,19 @@ class App extends React.Component {
    * create the cards for each fruit based on the user query on the search bar
    * by default all cards are created when no input is keyed by the user
    */
-  createCards = (array) =>{
-    return array.map(fruit =>{
-     const capitalizeName = fruit.type[0].toUpperCase() + fruit.type.slice(1);
+  createCards = (array) => {
+    return array.map((fruit) => {
+      const capitalizeName = fruit.type[0].toUpperCase() + fruit.type.slice(1);
       return (
-          <Card
-            key={fruit.id}
-            name={capitalizeName}
-            img={fruit.img}
-            calories={fruit.calories}
-          />
+        <Card
+          key={fruit.id}
+          name={capitalizeName}
+          img={fruit.img}
+          calories={fruit.calories}
+        />
       );
-    })
-  }
+    });
+  };
 
   render() {
     return (
